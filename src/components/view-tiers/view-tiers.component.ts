7a67ce0dcee1803dfcb00f0b8680f132f08ea0f2
Jack Bliss <j.bliss@campussociety.com>
2017-08-02 17:56:18 +0100

    Moved tiers view over to use 'database' table, removed tiers constant as its no longer used

--- src/components/view-tiers/view-tiers.component.ts
+++ src/components/view-tiers/view-tiers.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { Tiers } from '../../constants/tiers';
+import { Component, OnInit } from '@angular/core';
+import { TiersService } from '../../services/tiers.service';
 
 @Component({
     selector: 'view_tiers',
     styleUrls: ['./view-tiers.style.scss'],
     templateUrl: './view-tiers.template.html'
 })
-export class ViewTiersComponent{
-    tiers: object[] = Tiers;
+export class ViewTiersComponent implements OnInit{
+    constructor(
+        private tiersService: TiersService
+    ){  }
+    tiers: object[];
+
+    ngOnInit(){
+        this.tiersService.getTiers().then(tiers => {
+            this.tiers = tiers;
+        })
+    }
 };
